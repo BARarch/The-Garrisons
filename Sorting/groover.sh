528ddf15ea103d8b6a9d1e4697bbdcb54ce329da
BARarch <anthonyquivers@gmail.com>
2020-01-16 20:55:47 -0500

    Groover works with new REL and has been supplied to all garisons

---
+++ Sorting/groover.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+REL="../bash-pipe/"
+if [ -z "$1" ]; then
+  echo "what challenge are you grooving?"
+  read TITLE
+  echo "Groovy!"
+else
+  TITLE="$1" 
+fi
+
+if [ -z "$2" ]; then
+  echo "what is your command for $TITLE"
+  read COMMD
+else
+    COMMD="$2"
+fi
+
+if [ $COMMD == "--init" ]; then
+  bash $REL"init.sh" $TITLE
+
+elif [ $COMMD == "--case" ]; then
+  if [ -z "$3" ]; then
+    bash $REL"case.sh" $TITLE
+  else
+    bash $REL"case.sh" $TITLE $3
+  fi
+elif [ $COMMD == "--test" ]; then
+  if [ -z "$3" ]; then
+    bash $REL"test.sh" $TITLE
+  else
+    bash $REL"test.sh" $TITLE $3
+  fi
+elif [ $COMMD == "--submitted" ]; then
+  if [ -z "$3" ]; then
+    bash $REL"submitted.sh" $TITLE
+  else
+    bash $REL"submitted.sh" $TITLE $3
+  fi
+else
+  echo "Invalid Command"
+fi
+
