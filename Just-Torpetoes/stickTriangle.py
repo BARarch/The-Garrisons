6f192dffe47d9eaf01eeb89970f37c2bcb0ff71b
BARarch <anthonyquivers@gmail.com>
2020-01-18 19:45:09 -0500

    Submitted on 20-01-18 at 19:45:09

--- Just-Torpetoes/stickTriangle.py
+++ Just-Torpetoes/stickTriangle.py
@@ -1,12 +1,35 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
 #Date Started: 200118
 
-
+import math
 import os
+import random
+import re
 import sys
 
 # Complete the maximumPerimeterTriangle function below.
 def maximumPerimeterTriangle(sticks):
+    from collections import deque
+
+    S = deque(sorted(sticks))
+    A = S.pop()
+    B = S.pop()
+    C = S.pop()
+
+    if B + C > A:
+        return [C, B, A]
+
+    while S:
+        A = B
+        B = C
+        C = S.pop()
+
+        if B + C > A:
+            return [C, B, A]
+
+    return [-1, ]
+     
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
